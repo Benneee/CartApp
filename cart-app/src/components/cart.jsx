bfdad628ce750a6cce0b6c3b4114d3eb2f3cc1c9
Benedictiknkeonye@gmail.com <Benedictiknkeonye@gmail.com>
2019-03-21 02:56:44 +0100

    Stated on the first component

---
+++ cart-app/src/components/cart.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Cart extends Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default Cart;
--- cart-app/src/index.js
+++ cart-app/src/index.js
@@ -4,8 +4,9 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
+import Cart from "./components/cart";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Cart />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
